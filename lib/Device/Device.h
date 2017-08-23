f75249565f1e062224818bd135d6b573a6e1041d
orange-beans <cczhang@simtech.a-star.edu.sg>
2017-08-23 17:25:49 +0800

    Test vibration motor mixing performance

---
+++ lib/Device/Device.h
@@ -0,0 +1,12 @@
+#ifndef MBED_DEVICE_H
+#define MBED_DEVICE_H
+
+#define HIGH  1
+#define ON    1
+#define SET   1
+
+#define LOW   0
+#define OFF   0
+#define CLEAR 0
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include "mbed.h"
 
+// Local Constant Defines
+// TODO: How to move to another file?
+#define HIGH 1
+#define LOW  0
+
 DigitalOut LD3(PD_13);
 
+PwmOut LD4(PD_12);
+
 int main() {
+
+  LD4.period_ms(100);
+  LD4.write(0.5f);
+
   while(1) {
-    LD3 = 1;
-    wait(1.0f);
-    LD3 = 0;
-    wait(1.0f);
+    LD3 = HIGH;
+    wait(0.5f);
+    LD3 = LOW;
+    wait(0.5f);
   }
 }
