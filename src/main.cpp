b55c00ea98caa83155dd9cc24c2351477c92a1f9
orange-beans <cczhang@simtech.a-star.edu.sg>
2017-08-25 10:42:26 +0800

    Test on UART

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@
 #define HIGH 1
 #define LOW  0
 
+// Pin Assignments
 DigitalOut LD3(PD_13);
-
 PwmOut LD4(PD_12);
+
+// TODO: PA_9 as UART is not working
+Serial dev(PB_6, PB_7, 115200);
 
 int main() {
 
@@ -19,5 +22,6 @@ int main() {
     wait(0.5f);
     LD3 = LOW;
     wait(0.5f);
+    dev.printf("STM32 Testing\n");
   }
 }
